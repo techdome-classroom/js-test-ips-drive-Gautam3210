27212f58438331e7f982d7ba129f0195a52e16c1
Gautam Malviya <113026077+Gautam3210@users.noreply.github.com>
2024-04-26 13:58:08 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,22 @@
 function longestSubstring(s) {
 
 
-    // Implementation of longestSubstring function
+    let maxLength = 0;
+    let start = 0;
+    let charMap = {};
+
+    for (let end = 0; end < s.length; end++) {
+        const char = s[end];
+
+        if (charMap[char] !== undefined && charMap[char] >= start) {
+            start = charMap[char] + 1;
+        }
+
+        charMap[char] = end;
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
+
+    return maxLength;
 }
 
 module.exports = { longestSubstring };
