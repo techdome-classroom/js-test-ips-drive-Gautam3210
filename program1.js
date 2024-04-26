7a6fb0abc2aab2d39f4bcf43dda107e09ed33c40
Gautam Malviya <113026077+Gautam3210@users.noreply.github.com>
2024-04-26 13:57:09 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,7 +1,35 @@
 function smallestMissingPositiveInteger(nums) {
 
 
-    // Implement the function smallest_missing_positive_integer
+    let n = nums.length;
+
+    
+    let j = 0;
+    for (let i = 0; i < n; i++) {
+        if (nums[i] <= 0) {
+            [nums[i], nums[j]] = [nums[j], nums[i]]; 
+            j++;
+        }
+    }
+
+    
+    nums = nums.slice(j);
+
+    for (let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i]) - 1;
+        if (index < nums.length && nums[index] > 0) {
+            nums[index] = -nums[index];
+        }
+    }
+
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            return i + 1; 
+        }
+    }
+
+    return nums.length + 1; 
   
   }
   
